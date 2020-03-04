6f3817df5fdca4c3744dfd6456b55f6b362beac0
Michael Ingram <mpingram@uchicago.edu>
2020-03-04 16:53:51 -0600

    Pull zsh completions from github api

--- _config-diff-completions.zsh
+++ _config-diff-completions.zsh
@@ -1,8 +1,18 @@
-#compdef _config-diff ./config-diff
+#compdef _config-diff config-diff
+
+get_github_resources() {
+  local filter='[ .[] | if .type == "dir" then $repo + .name else empty end ] | join(" ")'
+  local cdis_manifest_resources=$(curl -sf "https://api.github.com/repos/uc-cdis/cdis-manifest/contents" | jq -r --arg repo cdis-manifest/ "$filter")
+  local gitops_dev_resources=$(curl -sf "https://api.github.com/repos/uc-cdis/gitops-dev/contents" | jq -r --arg repo gitops-dev/ "$filter")
+  local gitops_qa_resources=$(curl -sf "https://api.github.com/repos/uc-cdis/gitops-qa/contents" | jq -r --arg repo gitops-qa/ "$filter")
+
+  github_resources="$cdis_manifest_resources $gitops_dev_resources $gitops_qa_resources"
+  echo "$github_resources"
+}
 
 function _config-diff {
     # load environments list line-by-line into an array
-    suggestions=($(cat ./environments_list.txt))
-
-    compadd $suggestions
+    local -a suggestions
+    suggestions=($(get_github_resources))
+    _describe 'command' suggestions
 }
