0e714a5063d47420d58eb69f2fabdbd8ac1b5eb7
Michael Ingram <mpingram@uchicago.edu>
2020-03-05 15:30:39 -0600

    Update bash completion script

--- _config-diff-completions.sh
+++ _config-diff-completions.sh
@@ -1,12 +1,31 @@
+get_github_resources() {
+  local filter='[ .[] | if .type == "dir" then $repo + .name else empty end ] | join(" ")'
+  local cdis_manifest_resources=$(curl -sf "https://api.github.com/repos/uc-cdis/cdis-manifest/contents" | jq -r --arg repo cdis-manifest/ "$filter")
+  local gitops_dev_resources=$(curl -sf "https://api.github.com/repos/uc-cdis/gitops-dev/contents" | jq -r --arg repo gitops-dev/ "$filter")
+  local gitops_qa_resources=$(curl -sf "https://api.github.com/repos/uc-cdis/gitops-qa/contents" | jq -r --arg repo gitops-qa/ "$filter")
+
+  github_resources="$cdis_manifest_resources $gitops_dev_resources $gitops_qa_resources"
+  echo "$github_resources"
+}
+
 _script()
 {
-  _suggestions="$(cat ./environments_list.txt)"
+  # Look for completions in a temporary file; if they don't already exist,
+  # pull the completions from github.
+  completions_filename='/tmp/config-diff-completions.txt'
+  if [ ! -f $completions_filename ]
+    then
+      completions_file=$(mktemp $completions_filename)
+      echo "$(get_github_resources)" >> $completions_file
+  fi
+
+  suggestions=($(cat $completions_filename))
 
   local cur
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
-  COMPREPLY=( $(compgen -W "${_suggestions}" -- ${cur}) )
+  COMPREPLY=( $(compgen -W "${suggestions[*]}" -- ${cur}) )
 
   return 0
 }
-complete -o nospace -F _script ./config-diff
+complete -o nospace -F _script config-diff
